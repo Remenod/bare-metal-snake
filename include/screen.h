2f82a6e7492612dc7bbba7985113be43d957545c
Remenod <remenod@gmail.com>
2025-07-26 15:10:42 +0300

    move out vga funcs to own files

---
+++ include/screen.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <stdint.h>
+
+void put_char(int pos, char c);
+
+void put_string(int start_pos, char text[]);
+
+void clear_screen();
---
+++ src/lib/screen.c
@@ -0,0 +1,26 @@
+#include <screen.h>
+
+volatile uint16_t *vga = (volatile uint16_t *)0xB8000;
+
+void put_char(int pos, char c)
+{
+    vga[pos] = (0x0F << 8) | c;
+}
+
+void clear_screen()
+{
+    uint16_t blank = (0x07 << 8) | ' ';
+    for (int i = 0; i < 80 * 25; i++)
+    {
+        vga[i] = blank;
+    }
+}
+
+void put_string(int start_pos, char text[])
+{
+    int endpoint = start_pos + strlen(text);
+    for (int i = start_pos; i < endpoint; i++)
+    {
+        put_char(i, text[i - start_pos]);
+    }
+}
