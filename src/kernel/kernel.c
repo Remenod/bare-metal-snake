00bab6e2e286008df2ea412d4a5985b43db41539
Remenod <remenod@gmail.com>
2025-07-27 01:06:19 +0300

    improove text writing(for some reason)

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -14,15 +14,55 @@ void kernel_main()
     char hello[] = "Hello, world!";
 
     put_string(80 * 25 / 2 - strlen(hello) / 2, hello);
-    int i = 10;
+    int i = 3;
+    move_cursor(i);
     while (true)
     {
         char c;
         while (!(c = get_char()))
-            ;
-        put_char(i, c);
-        i++;
-    }
+        {
+            asm volatile("hlt");
+        }
 
-    asm volatile("hlt");
+        switch (c)
+        {
+        case '\b':
+            i--;
+            put_char(i, 0);
+            break;
+        case '\t':
+            put_string(i, "    ");
+            i += 4;
+            break;
+        case '\n':
+            if (i % 80 == 0)
+                i += 80;
+            else
+                i += 80 - (i % 80);
+            break;
+        case KEY_UP:
+            if (i >= 80)
+                i -= 80;
+            break;
+        case KEY_DOWN:
+            if (i <= 80 * 24)
+                i += 80;
+            break;
+        case KEY_LEFT:
+            if (i > 0)
+                i--;
+            break;
+        case KEY_RIGHT:
+            if (i <= 80 * 25)
+                i++;
+            break;
+        default:
+            put_char(i, c);
+            i++;
+            break;
+        }
+        move_cursor(i);
+        if (i > 80 * 25)
+            i = 3;
+    }
 }
--- src/lib/screen.c
+++ src/lib/screen.c
@@ -24,3 +24,17 @@ void put_string(int start_pos, char text[])
         put_char(i, text[i - start_pos]);
     }
 }
+
+void disable_vga_cursor()
+{
+    outb(0x3D4, 0x0A);
+    outb(0x3D5, 0x20);
+}
+
+void move_cursor(uint16_t pos)
+{
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, pos & 0xFF);
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, (pos >> 8) & 0xFF);
+}
