3a6a6251f553576baf40bfbe7207a1257de19905
Remenod <remenod@gmail.com>
2025-08-10 22:46:15 +0300

    arrlib

---
+++ include/arrlib.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stdbool.h>
+
+bool_t contains(int arr[], int size, int value);
---
+++ src/lib/arrlib.c
@@ -0,0 +1,9 @@
+#include <arrlib.h>
+
+bool_t contains(int arr[], int size, int value)
+{
+    for (int i = 0; i < size; i++)
+        if (arr[i] == value)
+            return true;
+    return false;
+}
