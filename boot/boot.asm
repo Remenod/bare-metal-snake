83fba4681d3099af70cb23bfb1717e5197be9619
Remenod <remenod@gmail.com>
2025-08-11 21:30:16 +0300

    automove

--- boot/boot.asm
+++ boot/boot.asm
@@ -47,7 +47,7 @@ disk_error:
 
 load_kernel:
   mov bx, KERNEL_OFFSET
-  mov dh, 11                       ; kernel sectors count
+  mov dh, 12                       ; kernel sectors count
   mov dl, [BOOT_DRIVE]
   call disk_load
   ret
--- src/apps/snake/snake.c
+++ src/apps/snake/snake.c
@@ -10,7 +10,9 @@
 
 char buf[12];
 
-uint16_t snake_size, tail_end_shift, head_pos, apple_pos;
+char last_key;
+uint64_t ticks_on_last_automove;
+uint16_t snake_size, tail_end_shift, head_pos, apple_pos, game_speed;
 uint16_t tail[2000];
 
 uint16_t get_random_odd_apple_pos()
@@ -23,9 +25,22 @@ uint16_t get_random_odd_apple_pos()
         return res;
 }
 
+void show_info(int data[6])
+{
+    put_string(1920, "                                                                               ");
+    put_string(1930, int_to_str(data[0], buf));
+    put_string(1940, int_to_str(data[1], buf));
+    put_string(1950, int_to_str(data[2], buf));
+    put_string(1960, int_to_str(data[3], buf));
+    put_string(1970, int_to_str(data[4], buf));
+    put_string(1980, int_to_str(data[5], buf));
+}
+
 void snake_main()
 {
     set_cursor_visibility(false);
+    ticks_on_last_automove = get_timer_ticks();
+    last_key = KEY_RIGHT;
     snake_size = 4;
     tail_end_shift = 0;
     tail[0] = 1;
@@ -33,6 +48,8 @@ void snake_main()
     tail[2] = 5;
     head_pos = 7;
     apple_pos = get_random_odd_apple_pos();
+    game_speed = 200;
+
     put_char(apple_pos - 1, 177);
     put_char(apple_pos, 177);
 
@@ -40,31 +57,40 @@ void snake_main()
     {
         char c;
         while (!(c = get_char()))
+        {
+            if (ticks_on_last_automove + game_speed < get_timer_ticks())
+            {
+                c = last_key;
+                ticks_on_last_automove = get_timer_ticks();
+                break;
+            }
+            show_info((int[]){ticks_on_last_automove, get_timer_ticks(), 0, apple_pos, head_pos, snake_size});
             asm volatile("hlt");
-
+        }
+        ticks_on_last_automove = get_timer_ticks();
         switch (c)
         {
         case 27:
             return;
-        case '\n':
-            snake_size++;
-            put_string(1980, int_to_str(snake_size, buf));
-            continue;
         case KEY_UP:
             if (head_pos >= 80)
                 head_pos -= 80;
+            last_key = c;
             break;
         case KEY_DOWN:
             if (head_pos <= 80 * 24)
                 head_pos += 80;
+            last_key = c;
             break;
         case KEY_LEFT:
             if (head_pos > 0)
                 head_pos -= 2;
+            last_key = c;
             break;
         case KEY_RIGHT:
             if (head_pos <= 80 * 25)
                 head_pos += 2;
+            last_key = c;
             break;
         }
 
@@ -83,7 +109,7 @@ void snake_main()
         }
 
         put_char(head_pos - 1, 219);
-        put_char(head_pos, 219); // 177 178 good also
+        put_char(head_pos, 219);
         put_char(tail[tail_end_shift] - 1, 0);
         put_char(tail[tail_end_shift], 0);
 
@@ -92,12 +118,6 @@ void snake_main()
         tail_end_shift++;
         tail_end_shift %= snake_size;
 
-        put_string(1920, "                                                                               ");
-        // put_string(1930, int_to_str(tail[0], buf));
-        // put_string(1940, int_to_str(tail[1], buf));
-        // put_string(1950, int_to_str(tail[2], buf));
-        put_string(1960, int_to_str(apple_pos, buf));
-        put_string(1970, int_to_str(head_pos, buf));
-        put_string(1980, int_to_str(snake_size, buf));
+        show_info((int[]){ticks_on_last_automove, get_timer_ticks(), 0, apple_pos, head_pos, snake_size});
     }
 }
