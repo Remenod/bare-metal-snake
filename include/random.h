f40f2224f231b95ca809d2d1ddbe68e3c8a76c59
Remenod <remenod@gmail.com>
2025-08-12 23:34:18 +0300

    random

---
+++ include/random.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef struct
+{
+    uint32_t seed;
+} Random;
+
+void random_init(Random *r, uint32_t seed);
+uint32_t random_next(Random *r);
+uint32_t random_next_bounded(Random *r, uint32_t max);
---
+++ src/lib/random.c
@@ -0,0 +1,22 @@
+#include <random.h>
+
+void random_init(Random *r, uint32_t seed)
+{
+    r->seed = seed;
+}
+
+uint32_t random_next(Random *r)
+{
+    r->seed ^= r->seed << 13;
+    r->seed ^= r->seed >> 17;
+    r->seed ^= r->seed << 5;
+    return r->seed;
+}
+
+uint32_t random_next_bounded(Random *r, uint32_t max)
+{
+    r->seed ^= r->seed << 13;
+    r->seed ^= r->seed >> 17;
+    r->seed ^= r->seed << 5;
+    return r->seed % max;
+}
