db3d63d2afb54d3d53791863379a657b8335f400
Remenod <remenod@gmail.com>
2025-08-19 20:26:06 +0300

    uniline ports functions

--- include/arch/x86/ports.h
+++ include/arch/x86/ports.h
@@ -2,35 +2,9 @@
 
 #include <lib/types.h>
 
-static inline uint8_t inb(uint16_t port)
-{
-    uint8_t ret;
-    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
-    return ret;
-}
-static inline void outb(uint16_t port, uint8_t data)
-{
-    asm volatile("outb %0, %1" : : "a"(data), "Nd"(port));
-}
-
-static inline uint16_t inw(uint16_t port)
-{
-    uint16_t ret;
-    asm volatile("inw %w1, %w0" : "=a"(ret) : "Nd"(port));
-    return ret;
-}
-static inline void outw(uint16_t port, uint16_t data)
-{
-    asm volatile("outw %w0, %w1" : : "a"(data), "Nd"(port));
-}
-
-static inline uint32_t inl(uint16_t port)
-{
-    uint32_t ret;
-    asm volatile("inl %w1, %0" : "=a"(ret) : "Nd"(port));
-    return ret;
-}
-static inline void outl(uint16_t port, uint32_t data)
-{
-    asm volatile("outl %0, %w1" : : "a"(data), "Nd"(port));
-}
+uint8_t inb(uint16_t port);
+void outb(uint16_t port, uint8_t data);
+uint16_t inw(uint16_t port);
+void outw(uint16_t port, uint16_t data);
+uint32_t inl(uint16_t port);
+void outl(uint16_t port, uint32_t data);
