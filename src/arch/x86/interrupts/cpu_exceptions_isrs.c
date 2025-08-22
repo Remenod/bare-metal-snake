930bdf7da546fc54c2efd106c1a4e692c7ad6462
Remenod <remenod@gmail.com>
2025-08-22 19:42:25 +0300

    likely needed isr here

--- src/arch/x86/interrupts/cpu_exceptions_isrs.c
+++ src/arch/x86/interrupts/cpu_exceptions_isrs.c
@@ -9,27 +9,35 @@
     }
 
 // add implemented isr vec here
-#define CPU_EXCEPTIONS X(0) X(1) X(3) X(4) X(6) X(8) X(13)
+#define ISR_TO_REGISTRATION X(0) X(1) X(3) X(4) X(6) X(7) X(8) X(11) X(13) X(16) X(19)
 
 DEFINE_UNSPECIAL_ISR(0, "Division by Zero");
-DEFINE_UNSPECIAL_ISR(1, "Debug");
-/* 2 Non-Maskable Interrupt (NMI) */
+DEFINE_UNSPECIAL_ISR(2, "NMI - Hardware Critical")
 DEFINE_UNSPECIAL_ISR(4, "Signed Int Overflow");
-/* 5 Bound Range Exceeded - (bound instruction) */
+DEFINE_UNSPECIAL_ISR(5, "Bound Range Exceeded");
 DEFINE_UNSPECIAL_ISR(6, "Invalid Opcode");
-/* 7 Device Not Available - FPU or coprocessor not available */
 DEFINE_UNSPECIAL_ISR(8, "Double Fault");
-/* 9 Coprocessor Segment Overrun (Intel only) */
+DEFINE_UNSPECIAL_ISR(9, "Coprocessor Segment Overrun");
+/* 10 Invalid TSS */ // TODO implement if TSS will be used
+DEFINE_UNSPECIAL_ISR(11, "Segment Not Present");
+DEFINE_UNSPECIAL_ISR(12, "Stack Segment Fault"); // TODO reimplement after Rings system will be implemented(also special common isr needed)
 DEFINE_UNSPECIAL_ISR(13, "General Protection Fault");
-/* 10 Invalid TSS */
-/* 11 Segment Not Present */
-/* 12 Stack Segment Fault */
-/* 14 Page Fault */
-/* 16 x87 FPU Floating-Point Error */
-/* 17 Alignment Check */
-/* 18 Machine Check - serious hardware error */
-/* 19 SIMD Floating-Point Exception */
-/* 20 Virtualization Exception */
+/* 14 Page Fault */ // TODO implement after paging will be implemented
+DEFINE_UNSPECIAL_ISR(16, "FPU Floating-Point Error");
+DEFINE_UNSPECIAL_ISR(17, "Unaligned data operation");
+DEFINE_UNSPECIAL_ISR(18, "Machine Check - CPU Hardware Failure");
+DEFINE_UNSPECIAL_ISR(19, "SIMD Floating-Point Error");
+/* 20 Virtualization Exception */ // TODO implement if Virtualization will be used(hell nah actually)
+
+/* Stateless means that isr can accept state,
+but it will only contain EIP CD EFLAGS and int_no, everything else will be zero.
+This is necessary in order to reduce interactions with the stack. */
+
+/* 1 Debug */
+void isr_1() // marked as stateless in cpu_interrupts.asm
+{
+    rsod_add_log("ISR1. No debug code provided");
+}
 
 /* Breakpoint (int3) */
 _Noreturn void isr_3() // marked as stateless in cpu_interrupts.asm
@@ -39,9 +47,17 @@ _Noreturn void isr_3() // marked as stateless in cpu_interrupts.asm
         ;
 }
 
+/* 7 Device Not Available - coprocessor not available */
+_Noreturn void isr_7(const cpu_state_t *state)
+{
+    rsod_add_log("ISR7. Probably FPU/SIMD Not Loaded");
+    show_rsod("Coprocessor Not Ready", state);
+    __builtin_unreachable();
+}
+
 void register_all_cpu_exceptions_isrs(void)
 {
 #define X(n) register_interrupt_handler(n, isr_##n);
-    CPU_EXCEPTIONS
+    ISR_TO_REGISTRATION
 #undef X
 }
