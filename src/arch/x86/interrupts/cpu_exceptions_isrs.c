4f2031eb7ae1ea0697331b221423e3ed51edab32
Remenod <remenod@gmail.com>
2025-08-22 20:09:30 +0300

    register all isr

--- src/arch/x86/interrupts/cpu_exceptions_isrs.c
+++ src/arch/x86/interrupts/cpu_exceptions_isrs.c
@@ -9,7 +9,8 @@
     }
 
 // add implemented isr vec here
-#define ISR_TO_REGISTRATION X(0) X(1) X(3) X(4) X(6) X(7) X(8) X(11) X(13) X(16) X(19)
+#define ISR_TO_REGISTRATION X(0) X(1) X(2) X(3) X(4) X(5) X(6) X(7) X(8) X(9) \
+    X(11) X(12) X(13) X(16) X(17) X(18) X(19)
 
 DEFINE_UNSPECIAL_ISR(0, "Division by Zero");
 DEFINE_UNSPECIAL_ISR(2, "NMI - Hardware Critical")
