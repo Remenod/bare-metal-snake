9c9b89a534240b791aeb7d77b91139a1f645f450
Remenod <remenod@gmail.com>
2025-08-23 18:13:11 +0300

    bounded random next

--- src/lib/random.c
+++ src/lib/random.c
@@ -15,8 +15,13 @@ uint32_t random_next(Random *r)
 
 uint32_t random_next_bounded(Random *r, uint32_t max)
 {
-    r->seed ^= r->seed << 13;
-    r->seed ^= r->seed >> 17;
-    r->seed ^= r->seed << 5;
-    return r->seed % max;
+    return random_next(r) % max;
+}
+
+uint32_t random_next_range(Random *r, uint32_t min, uint32_t max)
+{
+    if (max <= min)
+        return min;
+
+    return min + (random_next(r) % (max - min + 1));
 }
