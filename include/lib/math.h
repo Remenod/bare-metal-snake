b3a02339e780fcff9a8e9f54a6a2fe8bf163743b
Remenod <remenod@gmail.com>
2025-08-27 12:12:34 +0300

    trigonometry

--- include/lib/math.h
+++ include/lib/math.h
@@ -2,7 +2,17 @@
 
 #include <lib/types.h>
 
-#define PI 3.14159265f
+#define PI 3.14159265358979323846
+
+#define fmod(x, y)    \
+    _Generic((x, y),  \
+        float: fmodf, \
+        double: fmodd)(x, y)
+
+#define exp(val)     \
+    _Generic((val),  \
+        float: expf, \
+        double: expd)(val)
 
 #define sin(val)     \
     _Generic((val),  \
@@ -14,73 +24,65 @@
         float: cosf, \
         double: cosd)(val)
 
+#define tan(val)     \
+    _Generic((val),  \
+        float: tanf, \
+        double: tand)(val)
+
+#define ctg(val)     \
+    _Generic((val),  \
+        float: ctgf, \
+        double: ctgd)(val)
+
+#define asin(val)     \
+    _Generic((val),   \
+        float: asinf, \
+        double: asind)(val)
+
+#define acos(val)     \
+    _Generic((val),   \
+        float: acosf, \
+        double: acosd)(val)
+
+#define atan(val)     \
+    _Generic((val),   \
+        float: atanf, \
+        double: atand)(val)
+
+#define actg(val)     \
+    _Generic((val),   \
+        float: actgf, \
+        double: actgd)(val)
+
+#define sinh(val)     \
+    _Generic((val),   \
+        float: sinhf, \
+        double: sinhd)(val)
+
+#define cosh(val)     \
+    _Generic((val),   \
+        float: coshf, \
+        double: coshd)(val)
+
+#define tanh(val)     \
+    _Generic((val),   \
+        float: tanhf, \
+        double: tanhd)(val)
+
 #define abs(val)            \
     _Generic((val),         \
         int8_t: abs_int8,   \
         int16_t: abs_int16, \
         int32_t: abs_int32)(val)
 
-static inline float sinf(float x)
-{
-    while (x > PI)
-        x -= 2.0f * PI;
-    while (x < -PI)
-        x += 2.0f * PI;
+float fmodf(float x, float y);
+double fmodd(double x, double y);
 
-    float x2 = x * x;
-    float term1 = x;
-    float term2 = (x * x2) / 6.0f;
-    float term3 = (x * x2 * x2) / 120.0f;
-    float term4 = (x * x2 * x2 * x2) / 5040.0f;
+float sinf(float x);
+double sind(double x);
 
-    return term1 - term2 + term3 - term4;
-}
-static inline double sind(double x)
-{
-    while (x > PI)
-        x -= 2.0 * PI;
-    while (x < -PI)
-        x += 2.0 * PI;
-
-    double x2 = x * x;
-    double term1 = x;
-    double term2 = (x * x2) / 6.0f;
-    double term3 = (x * x2 * x2) / 120.0f;
-    double term4 = (x * x2 * x2 * x2) / 5040.0f;
-
-    return term1 - term2 + term3 - term4;
-}
-
-static inline float cosf(float x)
-{
-    while (x > PI)
-        x -= 2.0f * PI;
-    while (x < -PI)
-        x += 2.0f * PI;
-
-    float x2 = x * x;
-    float term1 = 1.0f;
-    float term2 = x2 / 2.0f;
-    float term3 = (x2 * x2) / 24.0f;
-    float term4 = (x2 * x2 * x2) / 720.0f;
-
-    return term1 - term2 + term3 - term4;
-}
-static inline double cosd(double x)
-{
-    while (x > PI)
-        x -= 2.0f * PI;
-    while (x < -PI)
-        x += 2.0f * PI;
-
-    double x2 = x * x;
-    double term1 = 1.0f;
-    double term2 = x2 / 2.0f;
-    double term3 = (x2 * x2) / 24.0f;
-    double term4 = (x2 * x2 * x2) / 720.0f;
-
-    return term1 - term2 + term3 - term4;
-}
+float cosf(float x);
+double cosd(double x);
 
 static inline int8_t abs_int8(int8_t val)
 {
--- src/lib/math.c
+++ src/lib/math.c
@@ -1,1 +1,126 @@
 #include <lib/math.h>
+
+float fmodf(float x, float y)
+{
+    if (y == 0.0f)
+        return 0.0f;
+    int sign = x < 0 ? -1 : 1;
+    x = (x < 0) ? -x : x;
+    y = (y < 0) ? -y : y;
+    int32_t n = (int32_t)(x / y);
+    return sign * (x - n * y);
+}
+double fmodd(double x, double y)
+{
+    if (y == 0.0)
+        return 0.0;
+    int sign = x < 0 ? -1 : 1;
+    x = (x < 0) ? -x : x;
+    y = (y < 0) ? -y : y;
+    int64_t n = (int64_t)(x / y);
+    return sign * (x - n * y);
+}
+
+float expf(float x)
+{
+    if (x == 0.0f)
+        return 1.0f;
+
+    int neg = x < 0.0f ? 1 : 0;
+    if (neg)
+        x = -x;
+
+    float term = 1.0f;
+    float sum = 1.0f;
+    for (int n = 1; n < 20; n++)
+    {
+        term *= x / n;
+        sum += term;
+        if (term < 1e-6f)
+            break;
+    }
+
+    return neg ? 1.0f / sum : sum;
+}
+double expd(double x)
+{
+    if (x == 0.0)
+        return 1.0;
+
+    int neg = x < 0.0 ? 1 : 0;
+    if (neg)
+        x = -x;
+
+    double term = 1.0;
+    double sum = 1.0;
+    for (int n = 1; n < 30; n++)
+    {
+        term *= x / n;
+        sum += term;
+        if (term < 1e-12)
+            break;
+    }
+
+    return neg ? 1.0 / sum : sum;
+}
+
+float sinf(float x)
+{
+    x = fmod(x, 2.0f * PI);
+    float x2 = x * x;
+    return x * (1 - x2 / 6.0f + x2 * x2 / 120.0f - x2 * x2 * x2 / 5040.0f);
+}
+double sind(double x)
+{
+    x = fmod(x, 2.0 * PI);
+    double x2 = x * x;
+    return x * (1 - x2 / 6.0 + x2 * x2 / 120.0 - x2 * x2 * x2 / 5040.0);
+}
+
+float cosf(float x)
+{
+    x = fmod(x, 2.0f * PI);
+    float x2 = x * x;
+    return 1 - x2 / 2 + x2 * x2 / 24 - x2 * x2 * x2 / 720.0f;
+}
+double cosd(double x)
+{
+    x = fmod(x, 2.0 * PI);
+    double x2 = x * x;
+    return 1 - x2 / 2 + x2 * x2 / 24 - x2 * x2 * x2 / 720.0;
+}
+
+float tanf(float x) { return sinf(x) / cosf(x); }
+double tand(double x) { return sind(x) / cosd(x); }
+
+float ctgf(float x) { return cosf(x) / sinf(x); }
+double ctgd(double x) { return cosd(x) / sind(x); }
+
+float asinf(float x) { return x + (x * x * x) / 6.0f + (3 * x * x * x * x * x) / 40.0f; }
+double asind(double x) { return x + (x * x * x) / 6.0 + (3 * x * x * x * x * x) / 40.0; }
+
+float acosf(float x) { return PI / 2 - asinf(x); }
+double acosd(double x) { return PI / 2 - asind(x); }
+
+float atanf(float x) { return x - x * x * x / 3.0f + x * x * x * x * x / 5.0f; }
+double atand(double x) { return x - x * x * x / 3.0 + x * x * x * x * x / 5.0; }
+
+float actgf(float x) { return PI / 2 - atanf(x); }
+double actgd(double x) { return PI / 2 - atand(x); }
+
+float sinhf(float x) { return (expf(x) - expf(-x)) / 2; }
+double sinhd(double x) { return (expd(x) - expd(-x)) / 2; }
+
+float coshf(float x) { return (expf(x) + expf(-x)) / 2; }
+double coshd(double x) { return (expd(x) + expd(-x)) / 2; }
+
+float tanhf(float x)
+{
+    float e2 = expf(2 * x);
+    return (e2 - 1) / (e2 + 1);
+}
+double tanhd(double x)
+{
+    double e2 = expd(2 * x);
+    return (e2 - 1) / (e2 + 1);
+}
