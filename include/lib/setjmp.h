bf9a1e8da38b0a96d015c2464ae66958c8b0fd5f
Remenod <remenod@gmail.com>
2025-09-02 21:50:45 +0300

    make `jmp_buf_t` fields `uint32_t`

--- include/lib/setjmp.h
+++ include/lib/setjmp.h
@@ -4,12 +4,12 @@
 
 typedef struct
 {
-    size_t EBP;
-    size_t EBX;
-    size_t ESI;
-    size_t EDI;
-    size_t ESP;
-    size_t EIP;
+    uint32_t EBP;
+    uint32_t EBX;
+    uint32_t ESI;
+    uint32_t EDI;
+    uint32_t ESP;
+    uint32_t EIP;
 } jmp_buf_t;
 
 int setjmp(jmp_buf_t *buf);
